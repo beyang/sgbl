532feeca7fe01d768912df3fa382452cf9051c50
Beyang Liu <beyang@sourcegraph.com>
2018-06-18 20:41:53 -0700

    sg open -copy

--- open.go
+++ open.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
@@ -16,40 +17,70 @@ func (c *Config) runOpen(args []string) {
 		f.PrintDefaults()
 	}
 	posFlag := f.String("pos", "", "the position at which to open the file, formatted as \"L${line}:${col}\"")
+	copyOnlyFlag := f.Bool("copy", false, "if set, then the URL will be copied to the clipboard instead of opened")
 	if err := f.Parse(args); err != nil {
 		f.Usage()
 		os.Exit(1)
 	}
 
 	pathArg := f.Arg(0)
-	if err := c.open(pathArg, *posFlag); err != nil {
+	if err := c.open(pathArg, *posFlag, *copyOnlyFlag); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		os.Exit(1)
 	}
 }
 
-func (c *Config) open(pathArg string, pos string) error {
+func (c *Config) getSourcegraphURL(pathArg string, pos string) (string, error) {
 	abspath, err := filepath.Abs(pathArg)
 	if err != nil {
-		return err
+		return "", err
 	}
 	finfo, err := os.Lstat(abspath)
 	if err != nil {
-		return err
+		return "", err
 	}
 	repoURI, err := evalRepoURI(abspath, finfo.IsDir())
 	if err != nil {
-		return err
+		return "", err
 	}
 	relPath, err := evalRelPathFromRepoRoot(abspath, finfo.IsDir())
 	if err != nil {
-		return err
+		return "", err
 	}
 	sgURL := evalFilePlusURL(
 		evalFileURL(c.sourcegraphURLForRepo(repoURI), repoURI, relPath, finfo.IsDir()),
 		"",
 		pos,
 	)
+	return sgURL, nil
+}
+
+func (c *Config) open(pathArg string, pos string, copyOnly bool) error {
+	sgURL, err := c.getSourcegraphURL(pathArg, pos)
+	if err != nil {
+		return err
+	}
+
+	if copyOnly {
+		switch runtime.GOOS {
+		case "linux":
+			cmd := exec.Command("xsel", "-ib")
+			cmd.Stdin = bytes.NewBufferString(sgURL)
+			if err := cmd.Run(); err != nil {
+				return fmt.Errorf("`xsel -ib` failed: %s", err)
+			}
+		case "darwin":
+			cmd := exec.Command("pbcopy")
+			cmd.Stdin = bytes.NewBufferString(sgURL)
+			if err := cmd.Run(); err != nil {
+				return fmt.Errorf("`pbcopy` failed: %s", err)
+			}
+		default:
+			return fmt.Errorf("OS %s unsupported", runtime.GOOS)
+		}
+		return nil
+	}
+
 	switch runtime.GOOS {
 	case "linux":
 		if err := exec.Command("xdg-open", sgURL).Run(); err != nil {
